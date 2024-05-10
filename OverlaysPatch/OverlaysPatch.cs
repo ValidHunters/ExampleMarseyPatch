9e0ab25f8af2323df5347b3b5ee4d09c4a27a642
noverd <dasfasru@gmail.com>
2024-05-10 18:56:34 +0300

    Doebka from Mary

--- OverlaysPatch/OverlaysPatch.cs
+++ OverlaysPatch/OverlaysPatch.cs
@@ -4,67 +4,33 @@ using HarmonyLib;
 public static class MarseyPatch
 {
     public static string Name = "Overlays Patch v2";
-    public static string Description = "Working now! The patch disables rendering of these overlays: DrunkOverlay, RainbowOverlay, BlurryVisionOverlay, BlindOverlay";
+
+    public static string Description =
+        "Working now! The patch disables rendering of these overlays: DrunkOverlay, RainbowOverlay, BlurryVisionOverlay, BlindOverlay";
+
     public static bool ignoreFields = true;
-}
-
-public static class MarseyLogger
-{
-    public enum LogType
-    {
-        INFO,
-        WARN,
-        FATL,
-        DEBG
-    }
-
-    public delegate void Forward(AssemblyName asm, string message);
-
-    public static Forward? logDelegate;
-
-    /// <see cref="BasePatch.Finalizer"/>
-    public static void Log(LogType type, string message)
-    {
-        logDelegate?.Invoke(Assembly.GetExecutingAssembly().GetName(), $"[{type.ToString()}] {message}");
-    }
 }
 
 [HarmonyPatch]
 public static class OverlaysPatch
 {
-    private static MethodInfo? GetOverlayBeforeDraw(string type)
-        // Disable overlays by disabling BeforeDraw. For example: https://github.com/space-wizards/space-station-14/blob/master/Content.Client/Drunk/DrunkOverlay.cs
+    /// <summary>
+    /// // Disable overlays by disabling BeforeDraw. For example: https://github.com/space-wizards/space-station-14/blob/master/Content.Client/Drunk/DrunkOverlay.cs
+    /// </summary>
+    /// <param name="type">Path to type</param>
+    /// <returns>MethodInfo</returns>
+    private static MethodInfo GetOverlayBeforeDraw(string type)
     {
-        var tp = AccessTools.TypeByName(type);
-        if (tp == null)
-        {
-            MarseyLogger.Log(MarseyLogger.LogType.WARN, $"Can't find overlay {type}.");
-            return null;
-        }
-
-        var method = AccessTools.Method(tp, "Draw");
-        if (method != null) return method;
-        MarseyLogger.Log(MarseyLogger.LogType.WARN, $"Can't find BeforeDraw method in {type}.");
-        return null;
+        return AccessTools.Method(AccessTools.TypeByName(type), "Draw");
     }
 
+    [HarmonyTargetMethods]
     private static IEnumerable<MethodBase> TargetMethods()
     {
-        var drunkOverlay = GetOverlayBeforeDraw("Content.Client.Drunk.DrunkOverlay");
-        if (drunkOverlay != null)
-            yield return drunkOverlay;
-
-        var rainbowOverlay = GetOverlayBeforeDraw("Content.Client.Drugs.RainbowOverlay");
-        if (rainbowOverlay != null)
-            yield return rainbowOverlay;
-
-        var blurryVisionOverlay = GetOverlayBeforeDraw("Content.Client.Eye.Blinding.BlurryVisionOverlay");
-        if (blurryVisionOverlay != null)
-            yield return blurryVisionOverlay;
-
-        var blindOverlay = GetOverlayBeforeDraw("Content.Client.Eye.Blinding.BlindOverlay");
-        if (blindOverlay != null)
-            yield return blindOverlay;
+        yield return GetOverlayBeforeDraw("Content.Client.Drunk.DrunkOverlay");
+        yield return GetOverlayBeforeDraw("Content.Client.Drugs.RainbowOverlay");
+        yield return GetOverlayBeforeDraw("Content.Client.Eye.Blinding.BlurryVisionOverlay");
+        yield return GetOverlayBeforeDraw("Content.Client.Eye.Blinding.BlindOverlay");
     }
 
     [HarmonyPrefix]
