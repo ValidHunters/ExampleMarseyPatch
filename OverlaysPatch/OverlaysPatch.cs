6a217099dfa05b371016d826fa7e35a2b184d0cf
Gagarinten-Noverdo <71782778+noverd@users.noreply.github.com>
2024-05-10 20:19:37 +0300

    Fix of overlaypatch (#3)
    * Overlay patch update: fix
    
    * Cringe fix
    
    * Fix
    
    * Update OverlaysPatch/OverlaysPatch.cs
    
    Co-authored-by: Mary <mary@thughunt.ing>
    
    * Naming
    
    * Update OverlaysPatch.cs
    
    * Doebka from Mary
    
    * Last fix
    
    * SKILL ISSUE
    
    * Update OverlaysPatch.cs
    
    * а вы думали
    
    ---------
    
    Co-authored-by: Mary <mary@thughunt.ing>

--- OverlaysPatch/OverlaysPatch.cs
+++ OverlaysPatch/OverlaysPatch.cs
@@ -3,62 +3,34 @@ using HarmonyLib;
 
 public static class MarseyPatch
 {
-    public static Assembly? RobustClient = null; 
-    public static Assembly? RobustShared = null;
-    public static Assembly? ContentClient = null;
-    public static Assembly? ContentShared = null;
-    
-    public static string Name = "Overlays Patch";
-    public static string Description = "Patches: DrunkOverlay, RainbowOverlay, BlurryVisionOverlay, BlindOverlay";
+    public static string Name = "Overlays patch";
+
+    public static string Description =
+        "Disables Draw on multiple overlays";
+
+    public static bool ignoreFields = true;
 }
 
+
 [HarmonyPatch]
-    public static class OverlaysPatch 
-    {        
-    	[HarmonyTargetMethods]
-    	private static IEnumerable<MethodBase> TargetMethods() 
-        {
-            Assembly? tp = MarseyPatch.RobustClient;
-            if (tp == null) yield break;
-            Type? DrunkOverlay = tp.GetType("Content.Client.Drunk.DrunkOverlay"); // Disable drunk by disabling Draw https://github.com/space-wizards/space-station-14/blob/master/Content.Client/Drunk/DrunkOverlay.cs
-            if (DrunkOverlay != null)
-            {
-            	MethodInfo? method = DrunkOverlay.GetMethod("Draw", BindingFlags.NonPublic | BindingFlags.Instance);
-            	if (method != null) 
-            	{
-            		yield return method;
-            	}
-            }
-            Type? RainbowOverlay = tp.GetType("Content.Client.Drugs.RainbowOverlay"); // Disable space drug effect by disabling method Draw https://github.com/space-wizards/space-station-14/blob/master/Content.Client/Drugs/RainbowOverlay.cs
-            if (RainbowOverlay != null)
-            {
-            	MethodInfo? method = RainbowOverlay.GetMethod("Draw", BindingFlags.NonPublic | BindingFlags.Instance);
-            	if (method != null) 
-            	{
-            		yield return method;
-            	}
-            }
-            Type? BlurryVisionOverlay = tp.GetType("Content.Client.Eye.Blinding.BlurryVisionOverlay"); // Disable the blurred-eye effect by turning off the Draw method https://github.com/space-wizards/space-station-14/blob/master/Content.Client/Eye
-            if (BlurryVisionOverlay != null)
-            {
-            	MethodInfo? method = BlurryVisionOverlay.GetMethod("Draw", BindingFlags.NonPublic | BindingFlags.Instance);
-            	if (method != null) 
-            	{
-            		yield return method;
-            	}
-            }
-            Type? BlindOverlay = tp.GetType("Content.Client.Eye.Blinding.BlindOverlay"); // Disable the effect of blinding by turning off the Draw method https://github.com/space-wizards/space-station-14/blob/master/Content.Client/Eye/Blinding/BlindOverlay.cs
+/// <summary>
+///  Disable overlays by disabling Draw. For example: https://github.com/space-wizards/space-station-14/blob/master/Content.Client/Drunk/DrunkOverlay.cs
+/// </summary>
+public static class OverlaysPatch
+{
+    private static MethodInfo GetOverlayDraw(string type)
     {
-            if (BlindOverlay != null)
-            {
-            	MethodInfo? method = BlindOverlay.GetMethod("Draw", BindingFlags.NonPublic | BindingFlags.Instance);
-            	if (method != null) 
-            	{
-            		yield return method;
-            	}
-            }
-        }
-	}
-	[HarmonyPrefix]
-	private static bool Prefix() => false;
+        return AccessTools.Method(AccessTools.TypeByName(type), "Draw");
+    }
+
+    private static IEnumerable<MethodBase> TargetMethods()
+    {
+        yield return GetOverlayDraw("Content.Client.Drunk.DrunkOverlay");
+        yield return GetOverlayDraw("Content.Client.Drugs.RainbowOverlay");
+        yield return GetOverlayDraw("Content.Client.Eye.Blinding.BlurryVisionOverlay");
+        yield return GetOverlayDraw("Content.Client.Eye.Blinding.BlindOverlay");
+    }
+
+    [HarmonyPrefix]
+    static bool Prefix() => false;
 }
