8b306febaf8903c9d51c131bd3058938b50d49b4
Gagarinten-Noverdo <71782778+noverd@users.noreply.github.com>
2024-05-12 12:02:11 +0300

    Command patch (#5)

---
+++ CommandPermissionPatch/CommandPermissionPatch.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+using HarmonyLib;
+
+public static class MarseyPatch
+{
+    public static string Name = "Command Permission Patch";
+
+    public static string Description =
+        "Disable permission checking on all commands";
+
+    public static bool ignoreFields = true;
+}
+
+[HarmonyPatch]
+/// <summary>
+/// Sets the ability to run a command to always true via PostFix: https://github.com/space-wizards/RobustToolbox/blob/master/Robust.Client/Console/ClientConsoleHost.cs#L198
+/// </summary>
+static class CommandPermissionPatch
+{
+    [HarmonyTargetMethod]
+    private static MethodBase TargetMethod()
+    {
+        return AccessTools.Method(AccessTools.TypeByName("Robust.Client.Console.ClientConsoleHost"), "CanExecute");
+    }
+
+    [HarmonyPostfix]
+    private static void Postfix(ref bool __result)
+    {
+        __result = true;
+    }
+}
